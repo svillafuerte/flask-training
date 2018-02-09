56b4febf67f8efedb34a7b8c9342b6295393ae9f
svillafuerte <svillafuerte@growthaccelerationpartners.com>
2018-02-09 09:23:23 -0600

    Chapter 5: Showing The Logged In User in Templates
    Link: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,6 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -21,7 +20,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
